9ffd9efd828b83e199eb301e83f67a8d82344994
emersonXavier <emerson.xavier@fatec.sp.gov.br>
2019-06-26 21:54:45 -0300

    Leitura de Script na TelaPesquisarProjeto

--- src/view/TelaPesquisarProjeto.java
+++ src/view/TelaPesquisarProjeto.java
@@ -14,6 +14,7 @@ import org.h2.bnf.context.DbTableOrView;
 import org.h2.command.ddl.TruncateTable;
 import org.h2.table.Table;
 import org.h2.table.TableBase;
+import org.h2.tools.RunScript;
 
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
@@ -43,6 +44,7 @@ import model.Projeto;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.FileReader;
 import java.awt.event.ActionEvent;
 
 /* org.h2.jdbc.JdbcSQLException: Column "codProj" not found [42122-197] */
@@ -70,6 +72,10 @@ public class TelaPesquisarProjeto {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					Connection con = DriverManager.getConnection(url);
+					Statement st = con.createStatement();
+					RunScript.execute(con, new FileReader("/lib/create-tables.sql"));
+					
 					TelaPesquisarProjeto window = new TelaPesquisarProjeto();
 					window.frmPesquisa.setVisible(true);
 				} catch (Exception e) {
